e3991f029b873680dc3f0e4930638d9b65ee9ce3
Jeras Bertine <jeras.bertine@epitech.eu>
2023-04-06 17:21:43 +0200

    feat

--- Workshop 2/src/index.js
+++ Workshop 2/src/index.js
@@ -2,18 +2,47 @@ import './style.css'
 import ReactDOM from 'react-dom/client'
 import { Canvas } from '@react-three/fiber'
 import Experience from './Experience.js'
+import React, { Component } from 'react';
+import { Canvas } from 'react-three-fiber';
 
-const root = ReactDOM.createRoot(document.querySelector('#root'))
+class ErrorBoundary extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { hasError: false };
+  }
 
-root.render(
+  static getDerivedStateFromError(error) {
+    return { hasError: true };
+  }
+
+  componentDidCatch(error, errorInfo) {
+    console.error('Error in React Three app:', error, errorInfo);
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return <h1>Something went wrong.</h1>;
+    }
+
+    return this.props.children;
+  }
+}
+
+function App() {
+  return (
     <Canvas
-        camera={ {
-            fov: 45,
-            near: 0.1,
-            far: 200,
-            position: [ - 4, 3, 6 ]
-        } }
+      camera={{
+        fov: 45,
+        near: 0.1,
+        far: 200,
+        position: [-4, 3, 6]
+      }}
     >
+      <ErrorBoundary>
         <Experience />
+      </ErrorBoundary>
     </Canvas>
-)
+  );
+}
+
+root.render(<App />);
